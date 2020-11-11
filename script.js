4141f9116bbb56aae64dee81120625a7c4003b26
Srobleri <srobleri@everis.com>
2020-11-11 15:09:38 +0100

    1 file modified

--- script.js
+++ script.js
@@ -4,19 +4,21 @@ const changeBackgroundButton = document.getElementById(
   "changeBackgroundButton"
 );
 
+const letters = getLetters();
+
+let color = "#";
+
 changeBackgroundButton.addEventListener("click", function() {
   this.style.backgroundColor = getRandomColor();
 });
 
 function getRandomColor() {
-  const letters = getLetters();
-  let color = '#';
   for (var i = 0; i < 6; i++) {
     color += letters[Math.floor(Math.random() * 16)];
   }
   return color;
 }
 
-function getLetters(){
-  return '0123456789ABCDEF';
+function getLetters() {
+  return "0123456789ABCDEF";
 }
